020cbadf0bd8902558aef05b788c8a8a2c8cf2f2
Goon <duy@wearetopgroup.com>
2023-08-01 11:44:39 +0700

    fix(config): docs and blog repo path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,13 +41,13 @@ const config = {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					editUrl: "https://github.com/digitopvn/diginext-docs/tree/main/docs/",
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					editUrl: "https://github.com/digitopvn/diginext-docs/tree/main/blog/",
 				},
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
