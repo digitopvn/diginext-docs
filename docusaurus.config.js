239324425a562e672e50a5c2b426966b5c0bcf2a
Goon <duy@wearetopgroup.com>
2023-08-01 13:27:13 +0700

    fix(footer): update credtis

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,7 +128,7 @@ const config = {
 						],
 					},
 				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+				copyright: `Copyright © ${new Date().getFullYear()} Diginext, Inc. Built with Docusaurus.`,
 			},
 			prism: {
 				theme: lightCodeTheme,
